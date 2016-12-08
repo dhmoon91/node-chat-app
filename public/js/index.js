dc34db9228de40f4b46d65af246a4b3dcad84882
stan <dhmoon91@gmail.com>
2016-12-08 05:44:39 -0500

    Add form messages/show incoming messages

--- public/js/index.js
+++ public/js/index.js
@@ -22,14 +22,20 @@ socket.on('newEmail', function (email) {
 
 socket.on('newMessage', function (message) {
   console.log('New message', message);
+
+  var li = jQuery('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+
+  jQuery('#messages').append(li);
 });
 
+/*
 socket.emit('createMessage', {
   from:'Frank',
   text: 'hi'
 }, function (data) {
   console.log('Got it', data);
-});
+});*/
 
 jQuery('#message-form').on ('submit', function (e) {
   e.preventDefault();
